63c0816828a0e1fce09a007d2b9c932e816f32e8
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-06-17 10:08:36 +0200

    Add files via upload

---
+++ src/global.js
@@ -0,0 +1,248 @@
+const speciesInput = document.getElementById("speciesInput")
+const speciesButton = document.getElementById("speciesButton")
+const speciesTable = document.getElementById("speciesTable")
+const speciesTableThead = document.getElementById("speciesTableThead")
+const speciesTableTbody = document.getElementById("speciesTableTbody")
+
+const abilitiesInput = document.getElementById("abilitiesInput")
+const abilitiesButton = document.getElementById("abilitiesButton")
+const abilitiesTable = document.getElementById("abilitiesTable")
+const abilitiesTableThead = document.getElementById("abilitiesTableThead")
+const abilitiesTableTbody = document.getElementById("abilitiesTableTbody")
+
+const movesInput = document.getElementById("movesInput")
+const movesButton = document.getElementById("movesButton")
+const movesTable = document.getElementById("movesTable")
+const movesTableThead = document.getElementById("movesTableThead")
+const movesTableTbody = document.getElementById("movesTableTbody")
+
+
+const headerAbilitiesName = document.querySelector("#abilitiesTableThead th.ability")
+const headerAbilitiesDescription = document.querySelector("#abilitiesTableThead th.description")
+
+const headerMovesMove = document.querySelector("#movesTableThead th.move")
+const headerMovesType = document.querySelector("#movesTableThead th.type")
+const headerMovesSplit = document.querySelector("#movesTableThead th.split")
+const headerMovesPower = document.querySelector("#movesTableThead th.power")
+const headerMovesAccuracy = document.querySelector("#movesTableThead th.accuracy")
+const headerMovesPP = document.querySelector("#movesTableThead th.PP")
+const headerMovesEffect = document.querySelector("#movesTableThead th.effect")
+
+const headerSpeciesID = document.querySelector("#speciesTableThead th.ID")
+const headerSpeciesSprite = document.querySelector("#speciesTableThead th.sprite")
+const headerSpeciesName = document.querySelector("#speciesTableThead th.species")
+const headerSpeciesTypes = document.querySelector("#speciesTableThead th.types")
+const headerSpeciesAbilities = document.querySelector("#speciesTableThead th.abilities")
+const headerSpeciesHP = document.querySelector("#speciesTableThead th.baseHP")
+const headerSpeciesAtk = document.querySelector("#speciesTableThead th.baseAttack")
+const headerSpeciesDef = document.querySelector("#speciesTableThead th.baseDefense")
+const headerSpeciesSpA = document.querySelector("#speciesTableThead th.baseSpAttack")
+const headerSpeciesSpD = document.querySelector("#speciesTableThead th.baseSpDefense")
+const headerSpeciesSpe = document.querySelector("#speciesTableThead th.baseSpeed")
+const headerSpeciesBST = document.querySelector("#speciesTableThead th.BST")
+const topButton = document.querySelector('.topButton')
+
+topButton.onclick = () => {
+    window.scrollTo({top: 0, behavior: 'auto'})
+    lazyLoading(reset = true)
+}
+
+
+speciesButton.addEventListener("click", () => {
+    if(!speciesButton.classList.contains("activeButton"))
+        tableButtonClick("species")
+})
+abilitiesButton.addEventListener("click", () => {
+    if(!abilitiesButton.classList.contains("activeButton"))
+        tableButtonClick("abilities")
+})
+movesButton.addEventListener("click", () => {
+    if(!movesButton.classList.contains("activeButton"))
+        tableButtonClick("moves")
+})
+
+
+
+
+
+
+headerAbilitiesName.addEventListener("click", () => {
+    if(headerAbilitiesName.classList.contains("th-sort-desc"))
+        sortTableByClassName(abilitiesTable, "ability", asc = true)
+    else
+        sortTableByClassName(abilitiesTable, "ability", asc = false)
+})
+headerAbilitiesDescription.addEventListener("click", () => {
+    if(headerAbilitiesDescription.classList.contains("th-sort-desc"))
+        sortTableByClassName(abilitiesTable, "description", asc = true)
+    else
+        sortTableByClassName(abilitiesTable, "description", asc = false)
+})
+
+
+
+
+
+
+headerMovesMove.addEventListener("click", () => {
+    if(headerMovesMove.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "move", asc = true)
+    else
+        sortTableByClassName(movesTable, "move", asc = false)
+})
+headerMovesType.addEventListener("click", () => {
+    if(headerMovesType.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "type", asc = true)
+    else
+        sortTableByClassName(movesTable, "type", asc = false)
+})
+headerMovesSplit.addEventListener("click", () => {
+    if(headerMovesSplit.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "split", asc = true)
+    else
+        sortTableByClassName(movesTable, "split", asc = false)
+})
+headerMovesPower.addEventListener("click", () => {
+    if(headerMovesPower.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "power", asc = true, parseINT = true)
+    else
+        sortTableByClassName(movesTable, "power", asc = false, parseINT = true)
+})
+headerMovesAccuracy.addEventListener("click", () => {
+    if(headerMovesAccuracy.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "accuracy", asc = true, parseINT = true)
+    else
+        sortTableByClassName(movesTable, "accuracy", asc = false, parseINT = true)
+})
+headerMovesPP.addEventListener("click", () => {
+    if(headerMovesPP.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "PP", asc = true, parseINT = true)
+    else
+        sortTableByClassName(movesTable, "PP", asc = false, parseINT = true)
+})
+headerMovesEffect.addEventListener("click", () => {
+    if(headerMovesEffect.classList.contains("th-sort-desc"))
+        sortTableByClassName(movesTable, "effect", asc = true)
+    else
+        sortTableByClassName(movesTable, "effect", asc = false)
+})
+
+
+
+
+
+
+
+headerSpeciesID.addEventListener("click", () => {
+    if(headerSpeciesID.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "ID", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "ID", asc = false, parseINT = true)
+})
+headerSpeciesSprite.addEventListener("click", () => {
+    if(headerSpeciesID.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "ID", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "ID", asc = false, parseINT = true)
+})
+headerSpeciesName.addEventListener("click", () => {
+    if(headerSpeciesName.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "species", asc = true)
+    else
+        sortTableByClassName(speciesTable, "species", asc = false)
+})
+headerSpeciesTypes.addEventListener("click", () => {
+    if(headerSpeciesTypes.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "types", asc = true)
+    else
+        sortTableByClassName(speciesTable, "types", asc = false)
+})
+headerSpeciesAbilities.addEventListener("click", () => {
+    if(headerSpeciesAbilities.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "abilities", asc = true)
+    else
+        sortTableByClassName(speciesTable, "abilities", asc = false)
+})
+headerSpeciesHP.addEventListener("click", () => {
+    if(headerSpeciesHP.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseHP", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseHP", asc = false, parseINT = true)
+})
+headerSpeciesAtk.addEventListener("click", () => {
+    if(headerSpeciesAtk.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseAttack", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseAttack", asc = false, parseINT = true)
+})
+headerSpeciesDef.addEventListener("click", () => {
+    if(headerSpeciesDef.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseDefense", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseDefense", asc = false, parseINT = true)
+})
+headerSpeciesSpA.addEventListener("click", () => {
+    if(headerSpeciesSpA.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseSpAttack", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseSpAttack", asc = false, parseINT = true)
+})
+headerSpeciesSpD.addEventListener("click", () => {
+    if(headerSpeciesSpD.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseSpDefense", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseSpDefense", asc = false, parseINT = true)
+})
+headerSpeciesSpe.addEventListener("click", () => {
+    if(headerSpeciesSpe.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "baseSpeed", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "baseSpeed", asc = false, parseINT = true)
+})
+headerSpeciesBST.addEventListener("click", () => {
+    if(headerSpeciesBST.classList.contains("th-sort-desc"))
+        sortTableByClassName(speciesTable, "BST", asc = true, parseINT = true)
+    else
+        sortTableByClassName(speciesTable, "BST", asc = false, parseINT = true)
+})
+
+
+
+
+
+
+
+speciesInput.addEventListener("input", e => {
+    const value = e.target.value
+    filterTableInput(value, [2, 3, 4], speciesTableTbody)
+})
+abilitiesInput.addEventListener("input", e => {
+    const value = e.target.value
+    filterTableInput(value, [0, 1], abilitiesTableTbody)
+})
+movesInput.addEventListener("input", e => {
+    const value = e.target.value
+    filterTableInput(value, [0, 1, 6], movesTableTbody)
+})
+
+
+window.onbeforeunload = () => {  
+    window.scrollTo(0, 0);
+}
+
+function isTouching(entries){
+    if(entries[0].isIntersecting)
+        lazyLoading(false)
+}
+
+
+const options = {
+        root: null,
+        rootMargins: "0px",
+        threshold: 0
+}
+const observer = new IntersectionObserver(isTouching, options)
+observer.observe(document.querySelector("#footer"))
+
+
+fetchData()
---
+++ src/utility.js
@@ -0,0 +1,226 @@
+function sanitizeString(string){
+    const regex = /^SPECIES_|^TYPE_|ABILITY_|^SPECIES_NONE|^ABILITY_NONE|^MOVE_|^SPLIT_|FLAG_|^EFFECT_/ig
+    const unsanitizedString = string.replace(regex, "")
+    let matchArray = unsanitizedString.match(/\w+/g)
+    if(matchArray !== null){
+        for (i = 0; i < matchArray.length; i++){
+            matchArray[i] = matchArray[i].split('_')
+            for (j = 0; j < matchArray[i].length; j++){
+                matchArray[i][j] = matchArray[i][j][0].toUpperCase() + matchArray[i][j].slice(1).toLowerCase()
+            }
+            matchArray[i] = matchArray[i].join(" ")
+        }
+        return matchArray.join("\n")
+    }
+    else
+        return unsanitizedString
+}
+
+
+
+
+
+
+
+
+async function displaySetup(){    
+    await footerP("")
+
+    await speciesTable.classList.remove("hide")
+    await speciesButton.classList.remove("hide")
+    await speciesInput.classList.remove("hide")
+
+    await abilitiesButton.classList.remove("hide")
+    await movesButton.classList.remove("hide")
+
+    await topButton.classList.remove("hide")
+    await speciesTable.classList.add("activeTable")
+    await speciesButton.classList.add("activeButton")
+    await speciesInput.classList.add("activeInput")    
+}
+
+async function fetchData(){
+    await forceUpdate()
+
+    await fetchMovesObj()
+    await fetchAbilitiesObj()
+    await fetchSpeciesObj()
+
+    await displaySetup()
+}
+
+
+async function forceUpdate(){
+    const update = 4
+    if(localStorage.getItem("update") != update){
+        await localStorage.clear()
+        await localStorage.setItem("update", update)
+        await footerP("Fetching data please wait... this is only run once")
+    }
+}
+
+
+
+
+
+function footerP(input){
+    if(input === "")
+        document.querySelectorAll("#footer > p").forEach(paragraph => paragraph.remove())
+
+    const paragraph = document.createElement("p")
+    const footer = document.getElementById("footer")
+    paragraph.innerText = input
+    footer.append(paragraph)
+}
+
+
+
+
+
+
+function sortTableByClassName(table, className, asc = true, parseINT = false) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+        let aColText = a.querySelector(`.${className}`).textContent.trim();
+        let bColText = b.querySelector(`.${className}`).textContent.trim();
+
+        if(parseINT)
+        {
+            aColText = parseInt(aColText)
+            if(isNaN(aColText))
+                aColText = 0
+            bColText = parseInt(bColText)
+            if(isNaN(bColText))
+                bColText = 0
+        }
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    lazyLoading(true)
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th.${className}`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th.${className}`).classList.toggle("th-sort-desc", !asc);
+}
+
+
+
+
+
+
+
+function filterTableInput(input, columns, tbody){
+    const inputArray = input.toLowerCase().split(" ")
+    let hideRows = {}
+    for (let j = 0; j < tbody.rows.length; j++){
+        let compareValue = ""
+        for (let i = 0; i < columns.length; i++){
+            compareValue += `${tbody.rows[j].cells[columns[i]].textContent.toLowerCase()} `
+        }
+        for (let i = 0; i < inputArray.length; i++){
+            inputArray[i] = inputArray[i].replace("-", " ")
+            if(!compareValue.includes(inputArray[i])){
+                hideRows[j] = "hide"
+                break
+            }
+        }
+    }
+    for(let i = 0; i < tbody.rows.length; i++){
+        if(hideRows[i] === "hide")
+            tbody.rows[i].classList.add("hide")
+        else
+            tbody.rows[i].classList.remove("hide")
+    }
+    lazyLoading(true)
+}
+
+
+
+
+
+
+
+function lazyLoading(reset = false){
+    const activeTables = document.getElementsByClassName("activeTable")
+    let rows = []
+    if(activeTables.length > 0)
+    {
+        rows = activeTables[0].tBodies[0].rows
+        let j = 0
+        for(let i = 0; i < rows.length; i++){
+            if(reset){
+                if(j <= 75){
+                    if(!rows[i].classList.contains("hide")){
+                        rows[i].classList.remove("hideTemp")
+                        j++
+                    }
+                }
+                else
+                    rows[i].classList.add("hideTemp")
+            }
+            else{
+                if(!rows[i].classList.contains("hide")){
+                    if(rows[i].classList.contains("hideTemp")){
+                        j++
+                        rows[i].classList.remove("hideTemp")
+                    }
+                }
+                if(j >= 75)
+                    break
+            }
+        }
+    }
+}
+
+
+
+
+
+
+async function tableButtonClick(input){
+    await lazyLoading(reset = true)
+    const activeTable = await document.querySelectorAll(".activeTable")
+    const activeButton = await document.querySelectorAll(".activeButton")
+    const activeInput = await document.querySelectorAll(".activeInput")
+
+    activeTable.forEach(table => {
+        table.classList.remove("activeTable")
+        table.classList.add("hide")
+    })
+
+
+    activeButton.forEach(button => {
+        button.classList.remove("activeButton")
+    })
+
+    activeInput.forEach(input => {
+        input.classList.remove("activeInput")
+        input.classList.add("hide")
+    })
+
+    const targetTable = await document.getElementById(`${input}Table`)
+    const targetButton = await document.getElementById(`${input}Button`)
+    const targetInput = await document.getElementById(`${input}Input`)
+
+    targetTable.classList.remove("hide")
+    targetTable.classList.add("activeTable")
+
+    targetButton.classList.add("activeButton")
+
+    targetInput.classList.remove("hide")
+    targetInput.classList.add("activeInput")
+}
