2b6f0fde8510b4b83c5943d97c4240e37739d563
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-07-08 21:44:16 +0200

    Add files via upload

--- src/global.js
+++ src/global.js
@@ -435,6 +435,12 @@ const options = {
 function footerIsTouching(entries){
     if(entries[0].isIntersecting){
         lazyLoading(false)
+        openCredits.classList.remove("hide")
+        update.classList.remove("hide")
+    }
+    else{
+        openCredits.classList.add("hide")   
+        update.classList.add("hide")
     }
 }
 
